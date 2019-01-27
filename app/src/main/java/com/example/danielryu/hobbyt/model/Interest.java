a1e6f218f001c63e76a5d7bfef7cc8491ac4970a
Dan_Y <xothix@gmail.com>
2019-01-26 17:03:47 -0800

    Created Interest enumeration

--- app/src/main/java/com/example/danielryu/hobbyt/model/Interest.java
+++ app/src/main/java/com/example/danielryu/hobbyt/model/Interest.java
@@ -1,4 +1,10 @@
 package com.example.danielryu.hobbyt.model;
 
-public class Interest {
+public enum Interest {
+    FITNESS("Fitness"), NATURE("Nature"), ENTERTAINMENT("Entertainment");
+    // Private to avoid unnecessary instantiation
+    private final String name;
+    private Interest(String name) {
+        this.name = name;
+    }
 }
